589a12e4eea5a09d25046a3726347e13119b5362
jouhar <67438817+jou-ali@users.noreply.github.com>
2023-10-03 23:19:14 +0530

    Common functions
    Common functions used in list,tuples and dictionary in python

---
+++ CommonFunctions.py
@@ -0,0 +1,36 @@
+#Common functions that can be used with LIST, TUPLES and DICTIONARY
+#------------------------------------------------------------------
+#---------------------------------LIST-----------------------------
+
+my_list = [1, 2, 3, 4, 5]
+
+#len()
+length = len(my_list)
+print(length)
+
+#append()
+my_list.append(6)
+print(my_list) 
+
+#extend()
+my_list.extend([7, 8])
+print(my_list)
+
+#insert().
+my_list.insert(1, 9)  
+print(my_list)  
+
+#remove()
+my_list.remove(2)
+print(my_list)
+
+#pop(): if index is not specified it will remove the last element
+popped_element = my_list.pop(1)
+print(popped_element)
+
+#------------------------------TUPLES-------------------------------
+#Add more
+
+
+#-----------------------------DICTIONARY----------------------------
+#Add more
